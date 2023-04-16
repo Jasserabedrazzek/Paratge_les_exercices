eb01259d5c7e6d5daaf0c8d3d40dde16f83c2d7c
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-04-16 03:53:59 +0200

    Update PythoN.py

--- PythoN.py
+++ PythoN.py
@@ -1,24 +1,18 @@
 import streamlit as st
-import json
 
-# Create a form for file upload
-with st.form("File Upload"):
-    uploaded_file = st.file_uploader("Choose a file")
+# Upload file
+uploaded_file = st.file_uploader("Choose a file")
 
-    # Add a submit button
-    submit_button = st.form_submit_button(label='Submit')
-
-# When the form is submitted
-if submit_button and uploaded_file is not None:
-    # Read the contents of the file
-    file_contents = uploaded_file.read()
-
-    # Process the file contents and create a JSON object
-    json_data = {'file_name': uploaded_file.name, 'file_data': file_contents.decode('utf-8')}
-
-    # Save the JSON object to a file
-    with open('file_data.json', 'w') as f:
-        json.dump(json_data, f)
-
-    # Display the JSON data in the Streamlit app
-    st.write('JSON Data:', json_data)
+# If file is uploaded
+if uploaded_file is not None:
+    # Get file content as bytes
+    file_content = uploaded_file.read()
+    # Display file content
+    st.write(file_content)
+    # Create download button
+    st.download_button(
+        label="Download file",
+        data=file_content,
+        file_name="uploaded_file.txt",
+        mime="text/plain"
+    )
